e8c93acce4a58f3ee4ee08b7b8d906619e530060
Skylar <0012089@student.isf.eud.hk>
2022-10-21 11:18:32 +0800

    I added so that it is just showing the question

--- view_riddles.py
+++ view_riddles.py
@@ -3,9 +3,9 @@ from all_riddles import riddles_json
 
 def main():
     riddles_list = riddles_json['riddles']
-    
+
     for riddle in riddles_list:
-        print(riddle)
+        print(riddle["question"])
 
 
 
